c1e8cee8ce95e8c463cbed8e8122af5ee96d0608
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-05-07 21:50:20 -0300

    Created some routes to test the DB

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const routes = express.Router();
+const uuidJs = require('uuid')
+const db = require('./database/connection');
 
-routes.get('/', (req, res) => {
-    res.json({
-        success: true,
-        message: 'Hello Node & Express!'
-    })
+routes.get('/ongs', async (req, res) => {
+    const data = await db.select('*').from('ong');
+    return res.json(data);
+});
+
+routes.get('/ongs/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = await db.select('*').from('ong').where({ id });
+
+    return res.json(data);
+});
+
+routes.post('/ongs', async (req, res) => {
+    const id = uuidJs.v4()
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const obj = await db('ong').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+    }, '*');
+
+    res.json(obj);
 });
 
 module.exports = routes
