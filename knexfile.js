b1c33491c75d4229c5a1917811f0b64c7083fa68
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-05-11 19:37:21 -0300

    Conexão com BD de acordo com o ambiente que a aplicação está rodando

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,24 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
+    client: 'postgresql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'root',
+      database : 'omnistack_11_db'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'src/database/migrations'
+    }  
+  },
+  production: {
     client: 'postgresql',
     connection: {
       connectionString: process.env.DATABASE_URL,
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,7 @@
+const env = process.env.ENV || 'development';
 const knex = require('knex');
 const knexConfig = require('../../knexfile');
 
-const connection = knex(knexConfig.development);
+const connection = knex(knexConfig[env]);
 
 module.exports = connection;
